38ed0d98c3fc886825462887bc2a107a3c57eb17
Thorbjørn Jelgren <tgj@edora.dk>
2023-07-12 17:48:23 +0200

    Create unit tests

---
+++ Tests/Unit/Api.Test/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,51 @@
+using Api.Controllers;
+using AutoFixture;
+using ControllerService.Weather;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Api.Test.Controllers;
+
+public class WeatherForecastControllerTests
+{
+    WeatherForecastController sut;
+    Mock<ILogger<WeatherForecastController>> loggerMock;
+    Mock<IWeatherForecastService> weatherForecastServiceMock;
+    Moq.Language.Flow.ISetup<IWeatherForecastService, Task<WeatherForecastModel>> setupMockGetWeatherForecastAsync;
+
+    Fixture fixture;
+
+    [SetUp]
+    public void Setup()
+    {
+        loggerMock = new Mock<ILogger<WeatherForecastController>>();
+
+        weatherForecastServiceMock = new Mock<IWeatherForecastService>();
+        setupMockGetWeatherForecastAsync = weatherForecastServiceMock.Setup(x => x.GetWeatherForecastAsync(It.IsAny<string>(), It.IsAny<int>()));
+
+        fixture = new Fixture();
+
+        sut = new WeatherForecastController(loggerMock.Object, weatherForecastServiceMock.Object);
+        sut.ControllerContext = new ControllerContext();
+        sut.ControllerContext.HttpContext = new DefaultHttpContext();
+    }
+
+    [Test]
+    public async Task GetWeatherForecast_ShouldCallWithCorrectParametersAndReturn200()
+    {
+        var cityParam = fixture.Create<string>();
+        var daysAheadParam = fixture.Create<int>();
+
+        setupMockGetWeatherForecastAsync
+            .ReturnsAsync(fixture.Create<WeatherForecastModel>())
+            .Verifiable();
+
+        await sut.GetWeatherForecast(cityParam, daysAheadParam);
+
+        Assert.That(sut.Response.StatusCode, Is.EqualTo(StatusCodes.Status200OK));
+
+        weatherForecastServiceMock.Verify(x => x.GetWeatherForecastAsync(cityParam, daysAheadParam), Times.Once);
+    }
+}
---
+++ Tests/Unit/ControllerService.Test/Weather/WeatherForecastServiceTests.cs
@@ -0,0 +1,69 @@
+using AutoFixture;
+using ControllerService.Weather;
+using Moq;
+using ServiceGateway.Weather;
+using static ServiceGateway.Weather.WeatherForecastResponse;
+
+namespace ControllerService.Test.Weather;
+
+public class WeatherForecastServiceTests
+{
+    WeatherForecastService sut;
+    Mock<IWeatherForecastGateway> WeatherForecastGatewayMock;
+    Moq.Language.Flow.ISetup<IWeatherForecastGateway, Task<WeatherForecastResponse?>> setupMockGetWeatherForecastAsync;
+
+    Fixture fixture;
+
+    [SetUp]
+    public void Setup()
+    {
+        WeatherForecastGatewayMock = new Mock<IWeatherForecastGateway>();
+        setupMockGetWeatherForecastAsync = WeatherForecastGatewayMock.Setup(x => x.GetAsync(It.IsAny<WeatherForecastRequest>()));
+
+        fixture = new Fixture();
+
+        sut = new WeatherForecastService(WeatherForecastGatewayMock.Object);
+    }
+
+    [Test]
+    public async Task GetWeatherForecastAsync_ShouldReturnCorrectModel()
+    {
+        var cityParam = fixture.Create<string>();
+        var daysAheadParam = new Random().Next(1,8);
+
+        var locationResponse = fixture
+            .Build<GeoLocation>()
+            .With(x => x.Name, cityParam)
+            .Create();
+
+        var forecastResponse = fixture
+            .Build<WeatherForecast>()
+            .With(x => x.Forecastday, fixture.CreateMany<Forecastday>(daysAheadParam).ToList())
+            .Create();
+
+        var gatewayResponse = fixture
+            .Build<WeatherForecastResponse>()
+            .With(x => x.Location, locationResponse)
+            .With(x => x.Forecast, forecastResponse)
+            .Create();
+
+        setupMockGetWeatherForecastAsync
+            .ReturnsAsync(gatewayResponse)
+            .Verifiable();
+
+        var result = await sut.GetWeatherForecastAsync(cityParam, daysAheadParam);
+
+        Assert.That(result.City, Is.EqualTo(cityParam));
+        Assert.That(result.Country, Is.EqualTo(locationResponse.Country));
+
+        var firstDayResult = result.Days.First();
+        var firstDayResponse = forecastResponse.Forecastday.First();
+        Assert.That(firstDayResult.Date, Is.EqualTo(firstDayResponse.Date));
+        Assert.That(firstDayResult.MaxtempC, Is.EqualTo(firstDayResponse.Day.MaxtempC));
+        Assert.That(firstDayResult.MintempC, Is.EqualTo(firstDayResponse.Day.MintempC));
+        
+        // Etc....
+
+        WeatherForecastGatewayMock.Verify(x => x.GetAsync(It.Is<WeatherForecastRequest>(x => x.City == cityParam && x.DaysAhead == daysAheadParam)), Times.Once);
+    }
+}
